131767868d433ba85ef445fbd66de22a2b3cf5b6
digiwidget <hrd.digimo@gmail.com>
2026-01-31 17:07:52 +0700

    Update dg-anime.js

--- dg-anime.js
+++ dg-anime.js
@@ -1,7 +1,7 @@
 (function () {
 
-  var ENTER_OFFSET = 120; 
-  var EXIT_OFFSET  = 150;  
+  var ENTER_OFFSET = 120;
+  var EXIT_OFFSET  = 80;
   var EXIT_DELAY   = 200;
 
   function handleReveal() {
@@ -11,17 +11,17 @@
       var rect = el.getBoundingClientRect();
       var vh   = window.innerHeight;
 
-      var enterPoint = rect.top < vh - ENTER_OFFSET;
-      var exitPoint  = rect.bottom < EXIT_OFFSET;
+      var isEntering = rect.top < vh - ENTER_OFFSET && rect.bottom > EXIT_OFFSET;
+      var isExitingTop = rect.top < -EXIT_OFFSET;
 
-      if (enterPoint && rect.bottom > EXIT_OFFSET) {
+      if (isEntering) {
         el.classList.add('active');
         el.classList.remove('out');
         el._dgExited = false;
         return;
       }
 
-      if (exitPoint && el.classList.contains('do') && el.classList.contains('active')) {
+      if (isExitingTop && el.classList.contains('do') && el.classList.contains('active')) {
         if (!el._dgExited) {
           el._dgExited = true;
           el.classList.add('out');
@@ -33,7 +33,7 @@
         return;
       }
 
-      if (!enterPoint) {
+      if (rect.top > vh) {
         el.classList.remove('active', 'out');
         el._dgExited = false;
       }
